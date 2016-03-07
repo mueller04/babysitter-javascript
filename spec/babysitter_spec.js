336da8d29b074f7a9b536db438644f8091171236
Mike Mueller <mikemueller04@hotmail.com>
2016-03-07 14:21:48 -0500

    pass the result given when start time is before 5 pm

---
+++ spec/babysitter_spec.js
@@ -0,0 +1,17 @@
+describe('When Babysitter is given a start and end time: ', function(){
+
+  var babysitter;
+
+  beforeEach(function(){
+    babysitter = new Babysitter();
+  });
+
+  describe('calculate returns', function(){
+
+    it('the result given when start time is before 5 pm', function(){
+      expect(babysitter.calculate(4)).toEqual('must start no later than 5 pm')
+    });
+  });
+
+
+});
---
+++ src/babysitter.js
@@ -0,0 +1,6 @@
+function Babysitter(){
+
+  Babysitter.prototype.calculate = function(number){
+    return 'must start no later than 5 pm';
+  };
+}
