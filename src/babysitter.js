a8ecc03a09982bed8044faad0717a3c068bb76b6
Mike Mueller <mikemueller04@hotmail.com>
2016-03-07 17:47:02 -0500

    refactored

--- src/babysitter.js
+++ src/babysitter.js
@@ -14,25 +14,29 @@ var endTimePay = 16.00;
       if (endTime <= 4) {
         endTime += 24;
       }
-
-      var total = 0;
-
-      for (i = beginTime + 1; i <= endTime; i++){
-        if (i <= bedTime) {
-          total += bedTimePay;
-        } else if (i <= midnight){
-          total += midnightpay;
-        } else {
-          total += endTimePay;
-        }
-      }
-      return total;
+      
+      return Babysitter.prototype.addHours(beginTime, endTime);
 
     } else {
       return Babysitter.prototype.validateTime(beginTime, endTime);
     }
 
   };
+
+  Babysitter.prototype.addHours = function(beginTime, endTime){
+    var total = 0;
+
+    for (i = beginTime + 1; i <= endTime; i++){
+      if (i <= bedTime) {
+        total += bedTimePay;
+      } else if (i <= midnight){
+        total += midnightpay;
+      } else {
+        total += endTimePay;
+      }
+    }
+    return total;
+  }
 
   Babysitter.prototype.validateTime = function(beginTime, endTime) {
     if (beginTime < 17 && beginTime > 4) {
